a90e5aa73d2cc779e1d47e846b23c56bea2d9a08
Tommy Boi <tomaarongrabowski@gmail.com>
2024-10-01 19:56:25 -0400

    Add .prettierrc file, install prettier-plugin-tailwindcss, add plugin to prettierrc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.scss";
-import Header from "@/components/layout/header/header";
-import Sidebar from "@/components/layout/sidebar/sidebar";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.scss';
+import Header from '@/components/layout/header/header';
+import Sidebar from '@/components/layout/sidebar/sidebar';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "velex",
-  description: "velex is the all in one Velocitor Solutions web platform",
+  title: 'velex',
+  description: 'velex is the all in one Velocitor Solutions web platform',
 };
 
 export default function RootLayout({
@@ -30,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Sidebar />
-        <Header />
-        {children}
+        <div className="flex flex-col">
+          <Sidebar />
+          <Header />
+          {children}
+        </div>
       </body>
     </html>
   );
