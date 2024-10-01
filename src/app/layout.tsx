9f70a3a5a801d195a8aefb9ab7693a0a7bcb27ff
tommygrabowski <tommy.grabowski@velocitor.com>
2024-10-01 11:06:07 -0400

    Add shadcn/ui to project, add shadcn/ui button, create header.tsx component file, add header from component file to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.scss";
+import Header from "@/components/layout/header/header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
