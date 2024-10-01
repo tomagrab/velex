54706508b58bd78a535c224d2df555fb6e3077fc
tommygrabowski <tommy.grabowski@velocitor.com>
2024-10-01 13:19:43 -0400

    Squashed commit of the following:
    commit 1b8c6715f24e29f1ae3677051fea904b0a249d4f
    Author: Tommy Grabowski <60988025+tomagrab@users.noreply.github.com>
    Date:   Tue Oct 1 12:45:11 2024 -0400
    
        Add shadcn/ui to project, add shadcn/ui button, create header.tsx component file, add header from component file to layout.tsx (#2)
    
        * Install SASS/SCSS and update existing CSS file to SCSS
    
        * Add shadcn/ui to project, add shadcn/ui button, create header.tsx component file, add header from component file to layout.tsx
    
        ---------
    
        Co-authored-by: tommygrabowski <tommy.grabowski@velocitor.com>

---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="flex flex-col items-center bg-gray-900 text-white py-4">
+            <h1 className="text-2xl font-bold">Next.js + Tailwind CSS</h1>
+        </header>
+    )
+}
