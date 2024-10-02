b20128cc2efa3b9c94bb4cbefa7b91cfdd42ecbf
tommygrabowski <tommy.grabowski@velocitor.com>
2024-10-02 15:48:28 -0400

    Normalize existing pages, adde metadata titles and descriptions, create Assets page, add Assets icon to sidebar menu

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,14 +1,23 @@
+import { Metadata } from 'next';
 import PageHeading from '@/components/layout/page-heading/page-heading';
 import Link from 'next/link';
 
-export default function Settings() {
+export const metadata: Metadata = {
+  title: 'Settings | velex',
+  description: 'Settings is where you can configure the application settings.',
+};
+
+export default function SettingsPage() {
   return (
     <div>
       <PageHeading title="Settings" />
       <div>
         <p>
           This is the settings page. Go to the{' '}
-          <Link href="/settings/configuration" className='paragraph-link'>Configuration</Link> page.
+          <Link href="/settings/configuration" className="paragraph-link">
+            Configuration
+          </Link>{' '}
+          page.
         </p>
       </div>
     </div>
