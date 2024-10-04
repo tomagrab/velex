477b742b2a06205e957d0b3e65d235c85a6fac8e
tommygrabowski <tommy.grabowski@velocitor.com>
2024-10-04 14:43:09 -0400

    Finish basic chat widget design, decide it looks kinda trash, oh well

--- src/components/layout/chat-widget/chat-widget-footer/chat-widget-footer.tsx
+++ src/components/layout/chat-widget/chat-widget-footer/chat-widget-footer.tsx
@@ -1,13 +1,13 @@
 import { Textarea } from '@/components/ui/textarea';
-import { Send } from 'lucide-react';
+import { ArrowUp } from 'lucide-react';
 
 export default function ChatWidgetFooter() {
   return (
-    <div className="border-t dark:last:bg-slate-800">
+    <div className="border-t bg-slate-800">
       <div className="flex items-center gap-2 p-2">
-        <Textarea className="resize-none bg-white text-black" />
-        <button className="flex items-center justify-center rounded-full bg-slate-800 p-2 dark:bg-background">
-          <Send className="text-white" />
+        <Textarea className="resize-none bg-white text-black dark:bg-slate-500 dark:text-white" />
+        <button className="flex items-center justify-center rounded-full bg-blue-500 p-2 dark:bg-background">
+          <ArrowUp className="text-white" />
         </button>
       </div>
     </div>
--- src/components/layout/chat-widget/chat-widget/chat-widget.tsx
+++ src/components/layout/chat-widget/chat-widget/chat-widget.tsx
@@ -18,7 +18,7 @@ export default function ChatWidget() {
         }
       }}
       className={cn(
-        'fixed z-50 overflow-hidden bg-slate-800 shadow-lg transition-all duration-300',
+        'fixed z-50 overflow-hidden shadow-lg transition-all duration-300',
         isOpen ? 'rounded-lg' : 'rounded-full',
         isOpen
           ? 'bottom-8 right-8 h-96 w-80 sm:h-[32rem] sm:w-96'
@@ -27,14 +27,14 @@ export default function ChatWidget() {
     >
       {/* Closed State */}
       {!isOpen && (
-        <div className="flex h-full w-full items-center justify-center">
-          <MessageCircle size={20} />
+        <div className="flex h-full w-full items-center justify-center bg-black dark:bg-slate-800">
+          <MessageCircle className="text-white" size={20} />
         </div>
       )}
 
       {/* Open State */}
       {isOpen && (
-        <div className="flex h-full w-full flex-col bg-white">
+        <div className="flex h-full w-full flex-col">
           <ChatWidgetHeader title="Chat" setIsOpen={setIsOpen} />
 
           <ChatWidgetBody />
